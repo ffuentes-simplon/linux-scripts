a061570e22c369cf3bf54c60d50bd32669b92183
ffuentes-simplon <128607988+ffuentes-simplon@users.noreply.github.com>
2023-03-23 10:02:08 +0100

    Mise à jour pour n'accepter que des nombres
    Retourne un code erreur si l'entrée n'est pas un nombre

--- guess.sh
+++ guess.sh
@@ -9,25 +9,30 @@ typeset -i tries=10
 typeset -i left=tries
 echo -e "----Trouvez le nombre entre 0 et 1000----\n"
 
-### Generate random number
-(( answer = RANDOM % 1000 + 1 ))
+# Générer un nombre entre 1 et 1000
+(( answer = RANDOM % 999 + 1 )) #RANDOM fourni une réponse à partir de 0, pour éviter ce cas, nous rajoutons 1
 
-### Play game
+#Règles du jeu
 while (( guess != answer && num < tries)); do
 	num=num+1
 	read -p "Entrez un nombre entre 1 et 1000 : " guess
-	if (( guess < answer )); then
-		echo "Le nombre est plus grand."
-                left="$tries-$num"
-                echo "Il vous reste $left essais"
-	elif (( guess > answer )); then
-		echo "Le nombre est plus petit."
-                left="$tries-$num"
-                echo "Il vous reste $left essais"
+	if [[ $guess =~ [0-9] ]]; then
+		if (( guess < answer )); then
+			echo "Le nombre est plus grand."
+                	left="$tries-$num"
+                	echo "Il vous reste $left essais"
+		elif (( guess > answer )); then
+			echo "Le nombre est plus petit."
+                	left="$tries-$num"
+                	echo "Il vous reste $left essais"
+		fi
+	else
+		echo "Veuillez n'entrer que des nombres"
+		exit 22 # Code erreur Invalid argument
 	fi
 done
-if ((guess == answer)); then
-   echo -e "Vous avez gagné en $num tentatives\n"
-elif ((num>=tries)); then
-   echo -e "Vous avez perdu. La bonne réponse était $answer\n"
-fi
+	if ((guess == answer)); then
+		echo -e "Vous avez gagné en $num tentatives\n"
+	elif ((num>=tries)); then
+		echo -e "Vous avez perdu. La bonne réponse était $answer\n"
+	fi
