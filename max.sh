ee25e33144e9334c3152e5e83dce991a6450734c
ffuentes-simplon <128607988+ffuentes-simplon@users.noreply.github.com>
2023-03-23 09:50:58 +0100

    Mise à jour pour n'accepter que les chiffres

--- max.sh
+++ max.sh
@@ -3,10 +3,15 @@
 # Trouver le plus grand des nombres en arguments
 args=("$@")
 
-#Indiquer au shell de prendre le retour chariot comme separateur de mots
-#afin de pouvoir la commande complete avec les pipes
-IFS=$'\n'
+if [[ "${args[@],,}" =~ [a-z] ]]; then #vérifier s il y a des lettres (tout passé en minuscule)
+	echo "Veuillez ne saisir que des nombres"
+	exit 22 #Code erreur Invalid argument
+else
+	#Indiquer au shell de prendre le retour chariot comme separateur de mots
+	#afin de pouvoir la commande complete avec les pipes
+	IFS=$'\n'
 
-#Afficher la premiere valeur (head) de la table (args) rangee par ordre
-#decroissant (sort -nr)
-echo "${args[*]}" | sort -nr | head -n1
+	#Afficher la premiere valeur (head) de la table (args) rangee par ordre
+	#decroissant (sort -nr)
+	echo "${args[*]}" | sort -nr | head -n1
+fi
