2eac53cf1dfafcc29b48e7d413fa7d1dac0f0f05
ffuentes-simplon <128607988+ffuentes-simplon@users.noreply.github.com>
2023-03-23 09:40:08 +0100

    Mise à jour pour vérifier le type d'argument
    Permet de contrôler le nombre d'arguments et le type avant de commencer

--- count.sh
+++ count.sh
@@ -2,6 +2,12 @@
 
 # Script qui compte jusqu'au nombre "argument"
 # Pour tout i entre 1 et "argument", incrémenté de 1
-for ((i = 1 ; i <= "$@" ; i++)); do
-  echo "$i" #Afficher "i"
-done
+
+if [[ $# == 1 && $1 =~ [0-9] ]];then # Vérifier s il y a un seul argument, et que c est un nombre
+	for ((i = 1 ; i <= "$@" ; i++)); do
+		echo "$i" #Afficher "i"
+	done
+else
+	echo "Veuillez entrer un argument numérique"
+	exit 22 #Code erreur Invalid argument
+fi
